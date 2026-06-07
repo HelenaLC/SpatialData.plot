e17c388fdd90962d0c2069aa9f81f7715f8a2432
HelenaLC <helena@crowell.eu>
2026-06-07 17:18:04 +0200

    remove not-unused code

--- R/utils.R
+++ R/utils.R
@@ -13,13 +13,6 @@
     } else stop("invalid 'x'")
 }
 
-#' .instance_key <- \(x) .spatialdata_attrs(x)$instance_key
-#' .region_key <- \(x) .spatialdata_attrs(x)$region_key
-#' .region <- \(x) .spatialdata_attrs(x)$region
-#' 
-#' #' @importFrom SingleCellExperiment int_colData
-#' .instance_ids <- \(x) int_colData(x)[[.instance_key(x)]]
-
 #' @importFrom grDevices col2rgb
 .str_is_col <- \(x) !inherits(tryCatch(error=\(e) e, col2rgb(x)), "error")
 
